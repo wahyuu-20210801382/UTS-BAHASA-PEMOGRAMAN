eb127159424eec534e91aab3cf91c8d7b81dc14c
wahyuu.yn <115616954+wahyuu-20210801382@users.noreply.github.com>
2022-11-10 14:23:40 +0700

    Add files via upload

---
+++ tugas 4.py
@@ -0,0 +1,11 @@
+## -----LIST---
+
+# KUMPULAN DATA KEPULAUWAN
+def DATAKEPULAUWAN ():
+    print("kalimantan")
+    print("kalimantan nourth")
+    print("kalimantan sourt")
+    print("sumatra west")
+    print("sumatra nourt")
+    print("sumatra sourt")
+DATAKEPULAUWAN ()
