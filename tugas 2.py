6d1e2e7589e9781791f18f4c3e193efe5b7f0bc5
wahyuu.yn <115616954+wahyuu-20210801382@users.noreply.github.com>
2022-11-10 14:22:14 +0700

    Add files via upload

---
+++ tugas 2.py
@@ -0,0 +1,6 @@
+# menghitung luas segitiga
+print("Menhitung luas segitiga")
+alas = float(input("masukkan alas :"))
+tinggi = float(input("Masukkan tinggi :"))
+luas = (0.5*alas*tinggi)
+print("\nluas segitiga adalah", luas)
